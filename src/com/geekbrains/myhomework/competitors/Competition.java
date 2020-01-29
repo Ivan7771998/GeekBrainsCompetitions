f27e23d314eb444be146b0f57b005bb2c014d56b
ivan <popov.ivan.dnr@gmail.com>
2020-01-29 23:12:15 +0300

    start_homework1

--- src/com/geekbrains/myhomework/competitors/Competition.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.myhomework.competitors;
-
-public interface Competition {
-    void run(double dist);
-    void jump(double height);
-    boolean onDistance();
-}
--- src/com/geekbrains/myhomework/obstacles/Obstacle.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.myhomework.obstacles;
-
-import com.geekbrains.myhomework.competitors.Competition;
-
-public abstract class Obstacle {
-    public abstract void justDoIt(Competition competition);
-}
